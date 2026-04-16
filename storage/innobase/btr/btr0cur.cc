29f18cb58e9543adbb91b2b6856c461bfd62245e
Alessandro Vetere <iminelink@gmail.com>
2026-04-16 16:12:27 +0200

    re-enable deferred page tracking in purge and update test results

--- storage/innobase/btr/btr0cur.cc
+++ storage/innobase/btr/btr0cur.cc
@@ -4445,7 +4445,6 @@ btr_cur_optimistic_delete(
 
 	ut_ad(flags == 0 || flags == BTR_CREATE_FLAG
 	      || flags == BTR_PURGE_DELETE_FLAG);
-	flags&= ~BTR_PURGE_DELETE_FLAG;
 	ut_ad(mtr->memo_contains_flagged(btr_cur_get_block(cursor),
 					 MTR_MEMO_PAGE_X_FIX));
 	ut_ad(mtr->is_named_space(cursor->index()->table->space));

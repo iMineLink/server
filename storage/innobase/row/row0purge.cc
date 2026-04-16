fbd0e9427d751d1d75e42c6bbef2841898632b9f
Alessandro Vetere <iminelink@gmail.com>
2026-04-16 14:51:50 +0200

    Revert "disable deferred page tracking in purge"
    This reverts commit 34e54bb3e8cf11990b90a43e645d7d08f2720da8.

--- storage/innobase/row/row0purge.cc
+++ storage/innobase/row/row0purge.cc
@@ -105,7 +105,6 @@ static inline void row_purge_record_deferred_page(
 	purge_node_t *node, dict_index_t *index,
 	const buf_block_t *block)
 {
-  return;
 	if (block->page.id().page_no() != index->page)
 		node->deferred_pages.emplace(index, block->page.id());
 }
@@ -119,7 +118,6 @@ static inline void row_purge_remove_deferred_page(
 	purge_node_t *node, dict_index_t *index,
 	const buf_block_t *block)
 {
-  return;
 	if (block->page.id().page_no() != index->page)
 		node->deferred_pages.erase({index, block->page.id()});
 }
